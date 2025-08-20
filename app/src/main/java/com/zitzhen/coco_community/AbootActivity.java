f9337d4edcd880e427bda0f97f4075d119329d96
oliver <oliverxiaozhen@outlook.com>
2025-08-21 02:38:29 +0800

    为AbootActivity添加隐私政策和检查更新按钮的点击事件

--- app/src/main/java/com/zitzhen/coco_community/AbootActivity.java
+++ app/src/main/java/com/zitzhen/coco_community/AbootActivity.java
@@ -17,12 +17,28 @@ public class AbootActivity extends AppCompatActivity {
 
         // 获取aboot.xml布局中的按钮
         Button agreementButton = findViewById(R.id.btn_user_agreement);
+        Button privacyButton = findViewById(R.btn_privacy_policy);
+        Button CheckupdateButton = findViewById(R.btn_Check_update);
 
         // 给按钮设置点击动作
         agreementButton.setOnClickListener(view -> {
             // 创建打开浏览器的意图
             Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://cc.zitzhen.cn/agreement/useragreement/"));
             startActivity(browserIntent);
+        });
+
+        privacyButton.setOnClickListener(view -> {
+            // 创建打开浏览器的意图
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://cc.zitzhen.cn/agreement/privacypolicy/"));
+            startActivity(browserIntent);
+        });
+
+        CheckupdateButton.setOnClickListener(view -> {
+            new AlertDialog.Builder(this)
+                    .setTitle("自动更新")
+                    .setMessage("自动更新已被禁止")
+                    .setPositiveButton("确定", null)
+                    .show();
         });
 
         // 后续在此新增代码...
