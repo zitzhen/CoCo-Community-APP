89038083c4f7a52cb2f1de7416f1ad15867530b6
oliver <oliverxiaozhen@outlook.com>
2025-12-13 16:11:36 +0800

    改main为kt，并留空Activity

---
+++ app/src/main/java/com/zitzhen/coco_community/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zitzhen.coco_community
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 空的Activity，不包含任何内容
+    }
+}
