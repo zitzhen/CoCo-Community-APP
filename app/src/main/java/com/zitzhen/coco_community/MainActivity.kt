c1ac3041c800391a86d3b456b5b84c52a9b28899
oliver <oliverxiaozhen@outlook.com>
2025-12-13 18:11:50 +0800

    临时跳转到登录页面

--- app/src/main/java/com/zitzhen/coco_community/MainActivity.kt
+++ app/src/main/java/com/zitzhen/coco_community/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.zitzhen.coco_community
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // 空的Activity，不包含任何内容
+        
+        // 跳转到登录页面
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        
+        // 结束MainActivity，这样用户按下返回键时不会回到空白的MainActivity
+        finish()
     }
 }
